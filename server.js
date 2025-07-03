576d649ef0d32fd778a855e130ebde3a172c5cd7
Nikhil <nikhil@example.com>
2025-07-03 12:28:52 +0530

    Fix .env and use process.env.MONGO_URI

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 // ✅ Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // ✅ MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
